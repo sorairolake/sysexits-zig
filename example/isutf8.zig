8d73cb9669a3bc418ce7de838e06baac3949991f
Shun Sakai <sorairolake@protonmail.ch>
2023-06-09 16:35:59 +0900

    Change behavior of `example/isutf8.zig`

--- example/isutf8.zig
+++ example/isutf8.zig
@@ -12,25 +12,24 @@ const std = @import("std");
 const sysexits = @import("sysexits");
 
 pub fn main() !u8 {
-    const alloc = std.heap.page_allocator;
-    const args = try std.process.argsAlloc(alloc);
-    defer std.process.argsFree(alloc, args);
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(!gpa.deinit());
+    const allocator = gpa.allocator();
 
-    var input: []u8 = undefined;
-    if (args.len < 2) {
-        const stdin = std.io.getStdIn().reader();
-        input = (try stdin.readUntilDelimiterOrEofAlloc(alloc, '\n', std.math.maxInt(usize))).?;
-    } else {
-        input = args[1];
-    }
+    const args = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, args);
+
+    const input = if (args.len < 2)
+        (try std.io.getStdIn().reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize))).?
+    else
+        try std.fs.cwd().readFileAlloc(allocator, args[1], std.math.maxInt(usize));
+    defer allocator.free(input);
 
     if (std.unicode.utf8ValidateSlice(input)) {
-        const stdout = std.io.getStdOut().writer();
-        try stdout.print("OK\n", .{});
+        try std.io.getStdOut().writer().print("OK\n", .{});
         return @enumToInt(sysexits.ExitCode.ok);
     } else {
-        const stderr = std.io.getStdErr().writer();
-        try stderr.print("Error: invalid UTF-8 sequence\n", .{});
+        try std.io.getStdErr().writer().print("Error: invalid UTF-8 sequence\n", .{});
         return @enumToInt(sysexits.ExitCode.data_err);
     }
 }
