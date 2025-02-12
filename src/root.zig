1aad4be640d9631fc7c247d67d37cd46e55204c5
Shun Sakai <sorairolake@protonmail.ch>
2025-02-12 23:06:21 +0900

    docs: Change to use inline links in doc comments

--- src/root.zig
+++ src/root.zig
@@ -3,9 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 //! The `sysexits` package provides the system exit code constants as defined
-//! by [`<sysexits.h>`].
-//!
-//! [`<sysexits.h>`]: https://man.openbsd.org/sysexits
+//! by [`<sysexits.h>`](https://man.openbsd.org/sysexits).
 
 pub const ExitCode = @import("exit_code.zig").ExitCode;
 
